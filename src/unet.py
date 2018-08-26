b76f55d7d1814925b74c055451f3971e3d0ceada
Joey Litalien <joey.litalien@gmail.com>
2018-08-26 03:22:49 -0400

    Added method to create image montage for easy comparison

--- src/unet.py
+++ src/unet.py
@@ -17,45 +17,39 @@ class UNet(nn.Module):
         self._block1 = nn.Sequential(
             nn.Conv2d(channels, 48, 3, padding=1),
             nn.Conv2d(48, 48, 3, padding=1),
-            nn.MaxPool2d(2)
-        )
+            nn.MaxPool2d(2))
 
         # Layers: enc_conv(i), pool(i); i=2..5
         self._block2 = nn.Sequential(
             nn.Conv2d(48, 48, 3, stride=1, padding=1),
-            nn.MaxPool2d(2)
-        )
+            nn.MaxPool2d(2))
 
         # Layers: enc_conv6, upsample5
         self._block3 = nn.Sequential(
             nn.Conv2d(48, 48, 3, stride=1, padding=1),
-            nn.ConvTranspose2d(48, 48, 3, stride=2, padding=1, output_padding=1)
+            nn.ConvTranspose2d(48, 48, 3, stride=2, padding=1, output_padding=1))
             # nn.Upsample(scale_factor=2, mode='nearest')
-        )
 
         # Layers: dec_conv5a, dec_conv5b, upsample4
         self._block4 = nn.Sequential(
             nn.Conv2d(96, 96, 3, stride=1, padding=1),
             nn.Conv2d(96, 96, 3, stride=1, padding=1),
-            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1)
+            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1))
             # nn.Upsample(scale_factor=2, mode='nearest')
-        )
 
         # Layers: dec_deconv(i)a, dec_deconv(i)b, upsample(i-1); i=4..2
         self._block5 = nn.Sequential(
             nn.Conv2d(144, 96, 3, padding=1),
             nn.Conv2d(96, 96, 3, padding=1),
-            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1)
+            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1))
             #nn.Upsample(scale_factor=2, mode='nearest')
-        )
 
         # Layers: dec_conv1a, dec_conv1b, dec_conv1c,
         self._block6 = nn.Sequential(
             nn.Conv2d(96 + channels, 64, 3, stride=1, padding=1),
             nn.Conv2d(64, 32, 3, stride=1, padding=1),
             nn.Conv2d(32, 3, 3, stride=1, padding=1),
-            nn.LeakyReLU(0.1)
-        )
+            nn.LeakyReLU(0.1))
 
         # Initialize weights
         self._init_weights()
