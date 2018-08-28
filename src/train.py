f22840dbe46655af119a6780938610cb8131c4e4
joeylitalien <joey.litalien@gmail.com>
2018-08-28 18:27:50 -0500

    Split data switch to train/dir so it's easier to specify where to train and validate (doesn't assume folder names anymore)

--- src/train.py
+++ src/train.py
@@ -15,7 +15,8 @@ def parse_args():
     parser = ArgumentParser(description='PyTorch implementation of Noise2Noise from Lehtinen et al. (2018)')
 
     # Data parameters
-    parser.add_argument('-d', '--data', help='dataset root path', default='./../data')
+    parser.add_argument('-t', '--train-dir', help='training set path', default='./../data/train')
+    parser.add_argument('-v', '--valid-dir', help='test set path', default='./../data/valid')
     parser.add_argument('--ckpt-save-path', help='checkpoint save path', default='./../ckpts')
     parser.add_argument('--ckpt-overwrite', help='overwrite model checkpoint on save', action='store_true')
     parser.add_argument('--report-interval', help='batch report interval', default=500, type=int)
@@ -35,7 +36,7 @@ def parse_args():
     # Corruption parameters
     parser.add_argument('-n', '--noise-type', help='noise type',
         choices=['gaussian', 'poisson', 'text', 'mc'], default='gaussian', type=str)
-    parser.add_argument('-v', '--noise-param', help='noise parameter (e.g. std for gaussian)', default=50, type=float)
+    parser.add_argument('-p', '--noise-param', help='noise parameter (e.g. std for gaussian)', default=50, type=float)
     parser.add_argument('-c', '--crop-size', help='random crop size', default=128, type=int)
     parser.add_argument('--clean-targets', help='use clean targets for training', action='store_true')
 
@@ -49,8 +50,8 @@ if __name__ == '__main__':
     params = parse_args()
 
     # Train/valid datasets
-    train_loader = load_dataset('train', params.train_size, params, shuffled=True)
-    valid_loader = load_dataset('valid', params.valid_size, params, shuffled=False)
+    train_loader = load_dataset(params.train_dir, params.train_size, params, shuffled=True)
+    valid_loader = load_dataset(params.valid_dir, params.valid_size, params, shuffled=False)
 
     # Initialize model and train
     n2n = Noise2Noise(params, trainable=True)
