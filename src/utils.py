e84f62b10f19f67b5b98f870be91b5ca161ada27
Joey Litalien <joey.litalien@gmail.com>
2018-08-29 02:46:29 -0400

    Fixed displaying issues with MC

--- src/utils.py
+++ src/utils.py
@@ -73,21 +73,32 @@ def plot_per_epoch(ckpt_dir, title, measurements, y_label):
     plot_fname = os.path.join(ckpt_dir, fname)
     plt.savefig(plot_fname, dpi=200)
     plt.close()
-    
-    
-    
-def reinhard_tonemap(rgb):
-    """Reinhard et al. (2002) tone mapping. (array version)"""
-    
-    return [255. * np.power(v / (1 + v), 1 / 2.2) for v in rgb]
-    
 
-def reinhard_tonemap_tensor(tensor):
-    """Reinhard et al. (2002) tone mapping (tensor version)."""
-    
-    return tensor#torch.pow(tensor / (1 + tensor), 1 / 2.2)
-    
-    
+
+def load_hdr(self, img_path):
+    """Converts OpenEXR image to PIL format."""
+
+    # Read OpenEXR file
+    if not OpenEXR.isOpenExrFile(img_path):
+        raise ValueError('HDR images must be in OpenEXR (.exr) format')
+    src = OpenEXR.InputFile(img_path)
+    pixel_type = Imath.PixelType(Imath.PixelType.FLOAT)
+    dw = src.header()['dataWindow']
+    size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+
+    # Load as float
+    rgb = [np.frombuffer(src.channel(c, pixel_type), dtype=np.float32) for c in 'RGB']
+    rgb8 = [Image.frombytes('F', size, c.tostring()).convert('L') for c in rgb]
+
+    return Image.merge('RGB', rgb8)
+
+
+def reinhard_tonemap(tensor):
+    """Reinhard et al. (2002) tone mapping."""
+
+    return torch.pow(tensor / (1 + tensor), 1 / 2.2)
+
+
 def psnr(source_denoised, target):
     """Computes peak signal-to-noise ratio.
     TODO: Find a pure PyTorch minibatch solution that also works on the GPU.
@@ -96,19 +107,20 @@ def psnr(source_denoised, target):
 
     s = source_denoised.detach()
     t = target.detach()
+
     s = np.array(tvF.to_pil_image(source_denoised.clamp(0, 1)))
     t = np.array(tvF.to_pil_image(target))
     return 10 * np.log10((255 ** 2) / ((s - t) ** 2).mean())
-    
-    
-def create_montage(img_name, save_path, noisy_t, denoised_t, clean_t, show, tonemap=False):
+
+
+def create_montage(img_name, save_path, source_t, denoised_t, clean_t, show):
     """Creates montage for easy comparison."""
 
     fig, ax = plt.subplots(1, 3, figsize=(9, 3))
     fig.canvas.set_window_title(img_name.capitalize()[:-4])
 
     # Bring tensors to CPU
-    noisy_t = noisy_t.cpu()
+    source_t = source_t.cpu()
     denoised_t = denoised_t.cpu()
     clean_t = clean_t.cpu()
 
@@ -121,17 +133,17 @@ def create_montage(img_name, save_path, noisy_t, denoised_t, clean_t, show, tone
     #     noisy = tvF.to_pil_image(noisy_t)
     #     denoised = tvF.to_pil_image(torch.clamp(denoised_t, 0, 1))
     #     clean = tvF.to_pil_image(clean_t)
-    
-    noisy = tvF.to_pil_image(noisy_t)
+
+    source = tvF.to_pil_image(source_t.narrow(0, 0, 3))
     denoised = tvF.to_pil_image(torch.clamp(denoised_t, 0, 1))
     clean = tvF.to_pil_image(clean_t)
 
     # Build image montage
-    psnr_vals = [psnr(noisy_t, clean_t), psnr(denoised_t, clean_t)]
+    psnr_vals = [psnr(source_t, clean_t), psnr(source_t, clean_t)]
     titles = ['Input: {:.2f} dB'.format(psnr_vals[0]),
               'Denoised: {:.2f} dB'.format(psnr_vals[1]),
               'Ground truth']
-    zipped = zip(titles, [noisy, denoised, clean])
+    zipped = zip(titles, [source, denoised, clean])
     for j, (title, img) in enumerate(zipped):
         ax[j].imshow(img)
         ax[j].set_title(title)
@@ -143,9 +155,9 @@ def create_montage(img_name, save_path, noisy_t, denoised_t, clean_t, show, tone
 
     # Save to files
     fname = os.path.splitext(img_name)[0]
-    noisy.save(os.path.join(save_path, '{}-noisy.png'.format(fname)))
-    denoised.save(os.path.join(save_path, '{}-denoised.png'.format(fname)))
-    fig.savefig(os.path.join(save_path, '{}-montage.png'.format(fname)), bbox_inches='tight')
+    source.save(os.path.join(save_path, f'{fname}-noisy.png'))
+    denoised.save(os.path.join(save_path, f'{fname}-denoised.png'))
+    fig.savefig(os.path.join(save_path, f'{fname}-montage.png'), bbox_inches='tight')
 
 
 class AvgMeter(object):
